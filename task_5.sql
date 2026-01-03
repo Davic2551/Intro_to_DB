0af666c000b338cbb00faf3c636e530c217a75bd
Davic225 <nuwagabadavis182@gmail.com>
2026-01-03 15:18:20 +0300

    Add task_5.sql to insert a single customer

---
+++ task_5.sql
@@ -0,0 +1,7 @@
+-- ==========================================================
+-- SCRIPT: task_5.sql
+-- DESCRIPTION: Insert a single customer into CUSTOMERS table
+-- ==========================================================
+
+INSERT INTO CUSTOMERS (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
